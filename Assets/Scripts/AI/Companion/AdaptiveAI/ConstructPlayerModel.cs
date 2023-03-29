fc7a7c895715a524b2a38dd07a02197a83031733
AJScott <as243879@falmouth.ac.uk>
2023-03-29 15:11:42 +0100

    feat: Maintainability
    -Added comments to the player model script
    -Updated readme

--- Assets/Scripts/AI/Companion/AdaptiveAI/ConstructPlayerModel.cs
+++ Assets/Scripts/AI/Companion/AdaptiveAI/ConstructPlayerModel.cs
@@ -21,6 +21,7 @@ public class ConstructPlayerModel : MonoBehaviour
 
     private void Start()
     {
+        //Setup the descriptors with values of 0
         descriptorValues.Add(Descriptor.Aggressive, 0);
         descriptorValues.Add(Descriptor.Counter, 0);
         descriptorValues.Add(Descriptor.Defensive, 0);
@@ -41,19 +42,25 @@ public class ConstructPlayerModel : MonoBehaviour
     private void Update()
     {
         DecayModels();
-        AdjustDisplay();
+        UpdateModel();
+
+        //Values for choosing next state if in current state for too long, this is intended to add variety
         explore += Time.deltaTime;
         currentSwitchCooldown += Time.deltaTime;
-        
     }
 
-    private void AdjustDisplay()
+    /// <summary>
+    /// Updates the player model with the descriptor of the highest value and updates the UI
+    /// </summary>
+    private void UpdateModel()
     {
         showValues.Clear();
 
+        //Sets up values for the highest state, overridden in loop
         Descriptor highestState = Descriptor.Null;
         float highestValue = 0;
 
+        //Empty item in case of null values
         DescriptorValue baseValue = new DescriptorValue();
         baseValue.descriptor = Descriptor.Null;
         baseValue.value = 0;
@@ -68,6 +75,7 @@ public class ConstructPlayerModel : MonoBehaviour
 
             showValues.Add(newValue);
 
+            //If the descriptor being checked is higher than the current highest descriptor, set the current highest descriptor to this one
             if (newValue.value > highestValue)
             {
                 highestValue = newValue.value;
@@ -77,12 +85,14 @@ public class ConstructPlayerModel : MonoBehaviour
 
         if (highestState != Descriptor.Null)
         {
+            //Changes to one of the next highest states if the player has been stuck in the same state for too long
             playerState = GetExploreState(highestState);
             trueState = highestState;
         }
 
         if (test) { return; }
 
+        //Updates the UI with the player state if it is active
         if (stateText != null)
         {
             stateText.text = "PS: " + playerState.ToString() + "TS: " + trueState.ToString();
@@ -93,14 +103,21 @@ public class ConstructPlayerModel : MonoBehaviour
     public float switchCooldown = 5;
     float currentSwitchCooldown = 0;
 
+    /// <summary>
+    /// Changes to one of the next highest states if the player has been stuck in the same state for too long
+    /// </summary>
+    /// <param name="newState">The true state the player is in</param>
+    /// <returns>The state to switch to, either the true state or one of the next highest states</returns>
     private Descriptor GetExploreState(Descriptor newState)
     {
+        //If the new state is different from the last state, then it has not been in the state too long, return the new state
         if (trueState != newState)
         {
             explore = 0;
             return newState;
         }
 
+        //If the cooldown for switching state in this way has not elapsed, return the existing player state
         if (currentSwitchCooldown < switchCooldown)
         {
             return playerState;
@@ -110,9 +127,11 @@ public class ConstructPlayerModel : MonoBehaviour
 
         float highestValue = descriptorValues[newState];
 
-        if (newState == Descriptor.Panic) // Makes it so that it will be less likely to explore states if the player is struggling
+        //Makes it so that it will be less likely to explore states if the player is struggling
+        if (newState == Descriptor.Panic)
             highestValue *= 1.5f;
 
+        //Loops through each state, checking if the distance between that and the new state is larger than the explore value, if so it adds it to the list of explore states
         foreach (var item in descriptorValues)
         {
             if (item.Key != newState)
@@ -126,6 +145,7 @@ public class ConstructPlayerModel : MonoBehaviour
             }
         }
 
+        //If there is at least 1 state that it can explore to, randomly choose between those states
         if (possibleStates.Count > 0)
         {
             Descriptor exploreState = possibleStates[Random.Range(0, possibleStates.Count)];
@@ -149,6 +169,9 @@ public class ConstructPlayerModel : MonoBehaviour
     public float cautiousDecay = 0.5f;
     public float panicDecay = 0.5f;
 
+    /// <summary>
+    /// Decays all descriptors by their decay values
+    /// </summary>
     private void DecayModels()
     {
         descriptorValues[Descriptor.Aggressive] = Mathf.Clamp(descriptorValues[Descriptor.Aggressive] - (aggressiveDecay * Time.deltaTime), 0, Mathf.Infinity);
@@ -166,6 +189,9 @@ public class ConstructPlayerModel : MonoBehaviour
     public List<CharacterController> currentTargets;
     public float currentTargetCastInterval = 0.6f;
 
+    /// <summary>
+    /// Gets the enemies the player is currently targetting
+    /// </summary>
     void CurrentTarget()
     {
         if (modelCharacter != null)
@@ -179,29 +205,42 @@ public class ConstructPlayerModel : MonoBehaviour
     public float attackCounterAdd = 7.5f;
     public float attackMissAdd = 3f;
 
+    /// <summary>
+    /// When the player attacks, add to the corresponding descriptors
+    /// </summary>
+    /// <param name="hit">Whether the player successfully hit their target</param>
     public void PlayerAttack(bool hit)
     {
-        descriptorValues[Descriptor.Aggressive] += attackHitAdd;
+        descriptorValues[Descriptor.Aggressive] += attackHitAdd; //Add to the aggressive descriptor
 
-        if (CheckCounter()) { descriptorValues[Descriptor.Counter] += attackCounterAdd; }
+        if (CheckCounter()) { descriptorValues[Descriptor.Counter] += attackCounterAdd; } //If the attack was made after a recent counter, add to the counter descriptor
 
-        if (!hit) { descriptorValues[Descriptor.Panic] += attackMissAdd; }
+        if (!hit) { descriptorValues[Descriptor.Panic] += attackMissAdd; } //If the attack missed, add to the panic descriptor
 
-        AdjustDisplay();
+        // ^ Still adds to the aggressive and counter descriptors on miss because we still want to know that the player is aggressive even if they are panicking
+
+        UpdateModel();
     }
 
     [Header("Parry Model Additions")]
     public float parrySuccessAdd = 5f;
     public float parryFailAdd = 7f;
 
+    /// <summary>
+    /// When the player parries, add to the corresponding descriptors
+    /// </summary>
+    /// <param name="hit">Whether the player is being targetted by an enemy</param>
     public void PlayerParry(bool beingAttacked)
     {
-        descriptorValues[Descriptor.Defensive] += parrySuccessAdd;
+        descriptorValues[Descriptor.Defensive] += parrySuccessAdd; //Add to the defensive descriptor
 
-        if (beingAttacked) { SetupCounter(counterWindowParry); }
-        else { descriptorValues[Descriptor.Panic] += parryFailAdd; }
+        if (beingAttacked) { SetupCounter(counterWindowParry); } //If the player was being attacked, set up a counter window
+        else { descriptorValues[Descriptor.Panic] += parryFailAdd; } //If the parry was made when no enemies were targetting the player, add to the panic descriptor
 
-        AdjustDisplay();
+        // ^ Still adds to the defensive descriptor on miss because we still want to know that the player is defensive even if they are panicking
+        //However, the counter window should not be opened if the player is not being attacked
+
+        UpdateModel();
     }
 
     [Header("Dodge Model Additions")]
@@ -210,31 +249,46 @@ public class ConstructPlayerModel : MonoBehaviour
     public float dodgeFailCAdd = 4f;
     public float dodgeSuccessDAdd = 3f;
     public float dodgeFailPAdd = 5f;
+
+    /// <summary>
+    /// When the player dodges, add to the corresponding descriptors
+    /// </summary>
+    /// <param name="hit">Whether the player is being targetted by an enemy</param>
     public void PlayerDodge(bool beingAttacked)
     {
-
+        //More complexity here because we want to add differing values to the descriptors on success/fail
         if (beingAttacked)
         {
+            //Add to cautious and defensive descriptors if being attacked
             descriptorValues[Descriptor.Cautious] += dodgeSuccessCAdd;
             descriptorValues[Descriptor.Defensive] += dodgeSuccessDAdd;
-            SetupCounter(counterWindowDodge);
+            SetupCounter(counterWindowDodge); //If the player was being attacked, set up a counter window
+
+            //Want to add to the defensive descriptor here because dodging is still a defensive action
         }
         else 
         {
+            //Add to cautious and panic descriptors if not being attacked
             descriptorValues[Descriptor.Cautious] += dodgeFailCAdd;
             descriptorValues[Descriptor.Panic] += dodgeFailPAdd;
+
+            //We still want to add to a cautious descriptor because some players might want to be mobile even if they
+            //are not being attacked since dodging can be faster than running if they need a quick burst of speed
         }
 
-        AdjustDisplay();
+        UpdateModel();
     }
 
     [Header("Hit Model Additions")]
     public float hitPanicAdd = 3f;
 
+    /// <summary>
+    /// When the player is hit, add to the panic descriptor
+    /// </summary>
     public void PlayerHit()
     {
         descriptorValues[Descriptor.Panic] += hitPanicAdd;
-        AdjustDisplay();
+        UpdateModel();
     }
 
     #endregion
