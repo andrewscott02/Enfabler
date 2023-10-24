8a3ef667e134129b0dcbdfd3ffc8b4b693a09c89
Andrew Scott <as243879@falmouth.ac.uk>
2023-10-24 16:40:00 +0100

    Enemies -New behaviour tree for ranged enemies that keeps them at a distance

---
+++ Assets/Scripts/Charactes/AI/BehaviourTree/BehaviourTrees/RangedBehaviourTree.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourTrees;
+
+public class RangedBehaviourTree : BehaviourTree
+{
+    protected override Node SetupTree()
+    {
+        //Debug.Log("Setting up basic BT for " + agent.name);
+
+        Node root = new Selector(
+
+            new Selector(
+
+                new Sequence(
+                    //If they are unable to make an attack, move to a point a short distance away
+                    new CannotAttack(agent, CharacterCombat.AttackType.SecondaryAttack),
+                    BaseBehaviours.MoveToRange(agent, 35f, false)
+                    ),
+
+                    //Checks if the closest enemy is within melee range and makes an attack if true
+                    BaseBehaviours.AttackClosestTarget(agent, true, agent.rangedDistance, CharacterCombat.AttackType.SecondaryAttack)
+                ),
+            
+            //Checks if the closest enemy is within melee range and makes an attack if true
+            BaseBehaviours.AttackClosestTarget(agent, true, agent.meleeDistance, CharacterCombat.AttackType.PrimaryAttack),
+            //Checks if the closest enemy is within sight range and moves towards it if true
+            BaseBehaviours.MoveToClosestTarget(agent, agent.distanceAllowance, false),
+            //If there are no targets, but the player is an ally, move to a point near the player
+            BaseBehaviours.FollowTarget(agent, agent.GetPlayer(), true),
+            //If there are no targets, move to a random point in the roam radius
+            BaseBehaviours.RoamToRandomPoint(agent)
+            );
+
+        return root;
+    }
+}
