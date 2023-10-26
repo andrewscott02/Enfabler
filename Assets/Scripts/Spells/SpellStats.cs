ac8afd8876cd54dac056cfc978847ddaaf6f2252
Andrew Scott <as243879@falmouth.ac.uk>
2023-10-26 11:29:52 +0100

    Enemies -Adjusted enemy stats and arena fights

---
+++ Assets/Scripts/Spells/SpellStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewSpell", menuName = "Spells/SpellStats", order = 0)]
+public class SpellStats : ScriptableObject
+{
+    public void CastSpell()
+    {
+
+    }
+}
--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -7,6 +7,7 @@ public class TimeManager : MonoBehaviour
     public static TimeManager instance;
 
     float defaultTimeScale = 1;
+    float currentTime = 1;
 
     // Start is called before the first frame update
     void Awake()
@@ -34,7 +35,10 @@ public class TimeManager : MonoBehaviour
     IEnumerator IResetTimeScale(float timeScale, float delay)
     {
         Time.timeScale = timeScale;
+        currentTime = Time.timeScale;
         yield return new WaitForSecondsRealtime(delay);
         Time.timeScale = defaultTimeScale;
+
+        currentTime = Time.timeScale;
     }
 }
