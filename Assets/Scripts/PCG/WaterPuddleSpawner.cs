0c857bceb0d23cf2ab8f114982404899477bf12b
Andrew Scott <as243879@falmouth.ac.uk>
2024-02-01 10:31:14 +0000

    PGC Dungeons -Water puddles under water dripping rocks

---
+++ Assets/Scripts/PCG/WaterPuddleSpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPuddleSpawner : MonoBehaviour
+{
+    public LayerMask layerMask;
+    public Object waterPuddlePrefab;
+
+    public Vector3 maxScaleModifier;
+
+    void Start()
+    {
+        Invoke("SpawnPuddle", 0.5f);
+    }
+
+    void SpawnPuddle()
+    {
+        RaycastHit hit;
+        if (Physics.Linecast(start: transform.position, end: transform.position + (Vector3.down * 15f), hitInfo: out hit, layerMask))
+        {
+            GameObject waterGO = Instantiate(waterPuddlePrefab, hit.point, Quaternion.Euler(hit.normal)) as GameObject;
+
+            Vector3 scale = new Vector3();
+            scale.x = Random.Range(waterGO.transform.localScale.x, waterGO.transform.localScale.x * maxScaleModifier.x);
+            scale.y = Random.Range(waterGO.transform.localScale.y, waterGO.transform.localScale.y * maxScaleModifier.y);
+            scale.z = Random.Range(waterGO.transform.localScale.z, waterGO.transform.localScale.z * maxScaleModifier.z);
+
+            waterGO.transform.localScale = scale;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+
+        RaycastHit hit;
+        if (Physics.Linecast(start: transform.position, end: transform.position + (Vector3.down * 15f), hitInfo: out hit, layerMask))
+        {
+            Gizmos.DrawLine(transform.position, hit.point);
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(hit.point, transform.position + (Vector3.down * 15f));
+        }
+        else
+        {
+            Gizmos.DrawLine(transform.position, transform.position + (Vector3.down * 15f));
+        }
+    }
+}
