5f31b8270cce18623968c5bd25359419bba44f5f
Andrew Scott <as243879@falmouth.ac.uk>
2024-02-25 10:42:01 +0000

    PCG Dungeons -Added dungeon manager to setup dungeon data

---
+++ Assets/Scripts/PCG/DungeonGeneration/DungeonManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonManager : MonoBehaviour
+{
+    public static GrammarsDungeonData grammarsDungeonData;
+}
